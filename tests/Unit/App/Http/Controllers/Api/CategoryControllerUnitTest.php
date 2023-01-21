686d36a758945908753beb7aed6329daec5f0660
vicente_nascimento <vicente19981@live.com>
2023-01-21 14:08:46 -0300

    test: ensure CategoryController list all categories

---
+++ tests/Unit/App/Http/Controllers/Api/CategoryControllerUnitTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Unit\App\Http\Controllers\Api;
+
+use App\Http\Controllers\Api\CategoryController;
+
+use Core\UseCase\Category\ListCategoriesUseCase;
+use Core\UseCase\DTO\Category\ListCategories\CategoriesListOutputDto;
+use Illuminate\Http\Request;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+
+class CategoryControllerUnitTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $mockRequest = Mockery::mock(Request::class);
+        $mockRequest
+            ->shouldReceive('get')
+            ->andReturn('test');
+
+        $mockDtoOutput = Mockery::mock(CategoriesListOutputDto::class, [
+            [], 1, 1, 1, 1, 1, 1,
+        ]);
+
+        $mockUseCase = Mockery::mock(ListCategoriesUseCase::class);
+        $mockUseCase->shouldReceive('execute')->once()->andReturn($mockDtoOutput);
+
+        $controller = new CategoryController();
+        $response = $controller->index($mockRequest, $mockUseCase);
+
+
+        dump($response);
+        $this->assertIsObject($response->resource);
+        $this->assertArrayHasKey('meta',$response->additional);
+    }
+}
