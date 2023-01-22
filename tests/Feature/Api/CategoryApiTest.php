2c4dc60ffe326d269b03600bd4107844d7d48123
vicente_nascimento <vicente19981@live.com>
2023-01-22 13:37:16 -0300

    test: list and store not found

---
+++ tests/Feature/Api/CategoryApiTest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use App\Models\Category;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
+use Tests\TestCase;
+
+class CategoryApiTest extends TestCase
+{
+    protected $endpoint = '/api/categories';
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_list_empty_categories()
+    {
+        $response = $this->getJson($this->endpoint);
+
+        $response->assertStatus(200);
+    }
+
+    public function test_list_all_categories()
+    {
+        Category::factory()->count(30)->create();
+
+        $response = $this->getJson($this->endpoint);
+
+        $response->assertStatus(200);
+
+
+        $response->assertJsonStructure([
+            'meta' => [
+                'total',
+                'current_page',
+                'last_page',
+                'first_page',
+                'per_page',
+                'to',
+                'from'
+            ]
+        ]);
+
+
+    }
+
+
+    public function test_list_paginate_categories()
+    {
+        Category::factory()->count(30)->create();
+
+        $response = $this->getJson("$this->endpoint?page=2");
+
+        $response->assertStatus(200);
+
+        $this->assertEquals(2,$response['meta']['current_page']);
+    }
+
+    public function test_list_category_notfound()
+    {
+        $response = $this->getJson("$this->endpoint/fake_value");
+
+        $response->dump();
+
+        $response->assertStatus(Response::HTTP_NOT_FOUND);
+    }
+
+
+    public function test_list_category()
+    {
+        $category = Category::factory()->create();
+        $response = $this->getJson("$this->endpoint/{$category->id}");
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJsonStructure([
+            'data' => [
+                'id',
+                'name',
+                'description',
+                'is_active',
+                'created_at'
+            ]
+        ]);
+
+        $this->assertEquals($category->id, $response['data']['id']);
+    }
+
+    public function test_validation_store()
+    {
+        $data = [];
+        $response = $this->postJson($this->endpoint,$data);
+
+        $response->dump();
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonStructure([
+            'message',
+            'errors' => [
+                'name'
+            ]
+        ]);
+    }
+}
