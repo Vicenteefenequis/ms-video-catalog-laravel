318f8ef4d32822a8ba1f9c61792bf2b8764b9b17
vicente_nascimento <vicente19981@live.com>
2023-01-22 13:27:36 -0300

    feat: adding current_page

--- src/Core/UseCase/DTO/Category/ListCategories/CategoriesListOutputDto.php
+++ src/Core/UseCase/DTO/Category/ListCategories/CategoriesListOutputDto.php
@@ -8,6 +8,7 @@ class CategoriesListOutputDto
     public function __construct(
         public array $items,
         public int $total,
+        public int $current_page,
         public int $last_page,
         public int $first_page,
         public int $per_page,
