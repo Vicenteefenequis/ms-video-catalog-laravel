5fa5c7f1f86476c006c226d98f992215542ce7e2
vicente_nascimento <vicente19981@live.com>
2023-01-21 23:38:15 -0300

    test: ensure CategoryController update category

--- tests/Feature/App/Http/Controllers/Api/CategoryControllerTest.php
+++ tests/Feature/App/Http/Controllers/Api/CategoryControllerTest.php
@@ -4,12 +4,14 @@ namespace Tests\Feature\App\Http\Controllers\Api;
 
 use App\Http\Requests\StoreCategoryRequest;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Requests\UpdateCategoryRequest;
 use App\Models\Category;
 use App\Models\Category as Model;
 use App\Repositories\Eloquent\CategoryEloquentRepository;
 use Core\UseCase\Category\CreateCategoryUseCase;
 use Core\UseCase\Category\ListCategoriesUseCase;
 use Core\UseCase\Category\ListCategoryUseCase;
+use Core\UseCase\Category\UpdateCategoryUseCase;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
@@ -63,7 +65,32 @@ class CategoryControllerTest extends TestCase
         $response = $this->controller->show(useCase: new ListCategoryUseCase($this->repository), id: $category->id);
 
         $this->assertInstanceOf(JsonResponse::class, $response);
-        $this->assertEquals(Response::HTTP_OK,$response->status());
+        $this->assertEquals(Response::HTTP_OK, $response->status());
 
+    }
+
+    public function test_update()
+    {
+        $category = Category::factory()->create();
+        $useCase = new UpdateCategoryUseCase($this->repository);
+
+        $request = new UpdateCategoryRequest();
+
+        $request->headers->set('content-type', 'application/json');
+        $request->setJson(new ParameterBag([
+            'name' => 'Updated'
+        ]));
+
+        $response = $this->controller->update(
+            request: $request,
+            useCase: $useCase,
+            id: $category->id,
+        );
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(Response::HTTP_OK, $response->status());
+        $this->assertDatabaseHas('categories', [
+            'name' => 'Updated'
+        ]);
     }
 }
