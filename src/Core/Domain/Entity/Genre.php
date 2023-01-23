9e9d48bf828b5ac2aac201df3cbec4297c2dcdae
vicente_nascimento <vicente19981@live.com>
2023-01-23 14:03:14 -0300

    feat: adding optional methods on order down

--- src/Core/Domain/Entity/Genre.php
+++ src/Core/Domain/Entity/Genre.php
@@ -10,12 +10,14 @@ use DateTime;
 class Genre
 {
     use MethodsMagicsTrait;
+
     public function __construct(
-        protected Uuid|null $id = null,
-        protected string $name,
-        protected bool $isActive = true,
-        protected DateTime|null $createdAt = null
-    ) {
+        protected string    $name,
+        protected ?Uuid     $id = null,
+        protected bool      $isActive = true,
+        protected ?DateTime $createdAt = null
+    )
+    {
         $this->id = $this->id ?? Uuid::random();
         $this->createdAt = $this->createdAt ?? new DateTime();
     }
