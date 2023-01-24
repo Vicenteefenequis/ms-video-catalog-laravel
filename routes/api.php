f47dc5b89b90353175f7d987828d3c02b2f6dc11
vicente_nascimento <vicente19981@live.com>
2023-01-24 16:21:44 -0300

    feat: create api resource genres

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,20 @@
 <?php
 
 use App\Http\Controllers\Api\{
-    CategoryController
+    CategoryController,
+    GenreController
 };
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('/categories', CategoryController::class);
 
+Route::apiResource(
+    name: '/genres',
+    controller: GenreController::class
+);
 
 Route::get('/', function () {
     return response()->json(['message' => 'success']);
 });
+
+
