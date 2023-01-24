d5e9bc83b159a7e0f4293af5b29f6c1a2b53d51f
vicente_nascimento <vicente19981@live.com>
2023-01-24 16:33:07 -0300

    tesT: ensure GenreApi store a genre with success

--- tests/Feature/Api/GenreApiTest.php
+++ tests/Feature/Api/GenreApiTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Api;
 
+use App\Models\Category;
 use App\Models\Genre as Model;
 use Illuminate\Http\Response;
 use Tests\TestCase;
@@ -15,7 +16,7 @@ class GenreApiTest extends TestCase
         $response = $this->getJson($this->endpoint);
 
         $response->assertStatus(Response::HTTP_OK);
-        $response->assertJsonCount(0,'data');
+        $response->assertJsonCount(0, 'data');
     }
 
     public function test_list_all()
@@ -24,7 +25,21 @@ class GenreApiTest extends TestCase
         $response = $this->getJson($this->endpoint);
 
         $response->assertStatus(Response::HTTP_OK);
-        $response->assertJsonCount(15,'data');
+        $response->assertJsonCount(15, 'data');
+    }
+
+    public function test_store()
+    {
+        $categories = Category::factory()->count(10)->create();
+
+        $response = $this->postJson($this->endpoint, [
+            'name' => 'new genre',
+            'is_active' => true,
+            'categories_ids' => $categories->pluck('id')->toArray(),
+        ]);
+
+        $response->assertStatus(Response::HTTP_CREATED);
+
     }
 
 }
