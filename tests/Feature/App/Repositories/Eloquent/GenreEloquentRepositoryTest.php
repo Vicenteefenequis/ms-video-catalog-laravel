904b0fe929b366145dd3486cafb595ee69de8d7b
vicente_nascimento <vicente19981@live.com>
2023-01-24 12:04:38 -0300

    test: ensure GenreEloquentRepository insert a genre with relationships

---
+++ tests/Feature/App/Repositories/Eloquent/GenreEloquentRepositoryTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Tests\Feature\App\Repositories\Eloquent;
+
+use App\Models\Category;
+use App\Models\Genre as Model;
+use Core\Domain\Entity\Genre as Entity;
+use App\Repositories\Eloquent\GenreEloquentRepository;
+use Core\Domain\Repository\GenreRepositoryInterface;
+use Tests\TestCase;
+
+class GenreEloquentRepositoryTest extends TestCase
+{
+    protected $repository;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->repository = new GenreEloquentRepository(new Model());
+    }
+
+    public function test_implements_interface()
+    {
+        $this->assertInstanceOf(GenreRepositoryInterface::class,$this->repository);
+    }
+
+    public function test_insert()
+    {
+        $entity = new Entity(name: 'Genre');
+        $response = $this->repository->insert($entity);
+
+        $this->assertEquals($entity->name,$response->name);
+        $this->assertEquals($entity->id(),$response->id());
+        $this->assertTrue($response->isActive);
+
+        $this->assertDatabaseHas('genres',[
+            'id' => $entity->id(),
+            'name' => $entity->name,
+            'is_active' => $entity->isActive
+        ]);
+    }
+
+    public function test_insert_deactivate()
+    {
+        $entity = new Entity(name: 'Genre');
+        $entity->deactivate();
+        $response = $this->repository->insert($entity);
+
+        $this->assertEquals($entity->name,$response->name);
+        $this->assertEquals($entity->id(),$response->id());
+        $this->assertFalse($response->isActive);
+
+        $this->assertDatabaseHas('genres',[
+            'id' => $entity->id(),
+            'name' => $entity->name,
+            'is_active' => $entity->isActive
+        ]);
+    }
+
+    public function test_insert_with_relationships()
+    {
+        $categories = Category::factory()->count(4)->create();
+
+        $entity = new Entity(name: 'teste');
+
+        foreach ($categories as $category) {
+            $entity->addCategory($category->id);
+        }
+
+        $response = $this->repository->insert($entity);
+
+        $this->assertDatabaseHas('genres',[
+            'id' => $response->id()
+        ]);
+
+
+        $this->assertDatabaseCount('categories',count($entity->categoriesId));
+
+    }
+}
