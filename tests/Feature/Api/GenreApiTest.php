d2f05c457ae490e2503827545b6e172f9c16eea6
vicente_nascimento <vicente19981@live.com>
2023-01-24 16:23:21 -0300

    test: ensure ListAllGenreE2E lists genres

---
+++ tests/Feature/Api/GenreApiTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use App\Models\Genre as Model;
+use Illuminate\Http\Response;
+use Tests\TestCase;
+
+class GenreApiTest extends TestCase
+{
+    protected $endpoint = '/api/genres';
+
+    public function test_list_all_genres_empty()
+    {
+        $response = $this->getJson($this->endpoint);
+
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJsonCount(0,'data');
+    }
+
+    public function test_list_all()
+    {
+        Model::factory()->count(20)->create();
+        $response = $this->getJson($this->endpoint);
+
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJsonCount(15,'data');
+    }
+
+}
