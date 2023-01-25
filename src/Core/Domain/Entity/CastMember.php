37defc835d18036980ccb3520feb0e48b86bdb12
vicente_nascimento <vicente19981@live.com>
2023-01-25 19:45:39 -0300

    feat: ensure CastMember received attributes

---
+++ src/Core/Domain/Entity/CastMember.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Core\Domain\Entity;
+
+
+use Core\Domain\Entity\Traits\MethodsMagicsTrait;
+use Core\Domain\Enum\CastMemberType;
+use Core\Domain\ValueObject\Uuid;
+use DateTime;
+
+class CastMember
+{
+    use MethodsMagicsTrait;
+
+    public function __construct(
+        protected string         $name,
+        protected CastMemberType $type,
+        protected ?Uuid          $id = null,
+        protected ?DateTime      $createdAt = null,
+    )
+    {
+        $this->id = $this->id ?? Uuid::random();
+        $this->createdAt = $this->createdAt ?? new DateTime();
+    }
+}
