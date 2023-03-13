8fa6cb37b6dde50cc8fc2dd2f12042599c09f0af
Vicente Junior <vicente19981@live.com>
2023-03-13 17:46:56 -0300

    refactor: to using trait methods

--- tests/Unit/UseCase/Genre/ListGenresUseCaseUnitTest.php
+++ tests/Unit/UseCase/Genre/ListGenresUseCaseUnitTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Unit\UseCase\Genre;
 
-use Core\Domain\Repository\PaginationInterface;
 use Core\UseCase\DTO\Genre\List\ListGenresInputDto;
 use Core\Domain\Repository\GenreRepositoryInterface;
 use Core\UseCase\DTO\Genre\List\ListGenresOutputDto;
@@ -10,9 +9,11 @@ use Core\UseCase\Genre\ListGenresUseCase;
 use Mockery;
 use PHPUnit\Framework\TestCase;
 use stdClass;
+use Tests\Unit\UseCase\UseCaseTrait;
 
 class ListGenresUseCaseUnitTest extends TestCase
 {
+    use UseCaseTrait;
     /**
      * A basic unit test example.
      *
@@ -39,18 +40,5 @@ class ListGenresUseCaseUnitTest extends TestCase
 
 
 
-    protected function mockPagination(array $items = [])
-    {
-        $this->mockPagination = Mockery::mock(stdClass::class, PaginationInterface::class);
-        $this->mockPagination->shouldReceive('items')->andReturn($items);
-        $this->mockPagination->shouldReceive('total')->andReturn(0);
-        $this->mockPagination->shouldReceive('currentPage')->andReturn(0);
-        $this->mockPagination->shouldReceive('firstPage')->andReturn(0);
-        $this->mockPagination->shouldReceive('lastPage')->andReturn(0);
-        $this->mockPagination->shouldReceive('perPage')->andReturn(0);
-        $this->mockPagination->shouldReceive('to')->andReturn(0);
-        $this->mockPagination->shouldReceive('from')->andReturn(0);
 
-        return $this->mockPagination;
-    }
 }
