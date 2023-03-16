c8f5a25d07e827ced23f91c1433ebeab8446e3d4
Vicente Junior <vicente19981@live.com>
2023-03-16 12:00:10 -0300

    test: ensure cast member model initialize correctly

---
+++ tests/Unit/App/Models/CastMemberUnitTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Tests\Unit\App\Models\ModelTestCase;
+
+class CastMemberUnitTest extends ModelTestCase
+{
+
+    protected function model(): Model
+    {
+        return new CastMember();
+    }
+
+
+    protected function traits(): array
+    {
+        return [
+            HasFactory::class,
+            SoftDeletes::class,
+        ];
+    }
+
+    protected function fillables(): array
+    {
+       return [
+           'id',
+           'name',
+           'type',
+       ];
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'id' => 'string',
+            'deleted_at' => 'datetime'
+        ];
+    }
+}
