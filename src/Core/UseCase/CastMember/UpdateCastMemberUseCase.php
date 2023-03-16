d8556b3b81dbe1a4973502f1ad588583c21fb98d
Vicente Junior <vicente19981@live.com>
2023-03-16 10:47:32 -0300

    feat: create UpdateCastMemberUseCase

---
+++ src/Core/UseCase/CastMember/UpdateCastMemberUseCase.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace Core\UseCase\CastMember;
+
+
+use Core\Domain\Repository\CastMemberRepositoryInterface;
+use Core\UseCase\DTO\CastMember\Update\{
+    UpdateCastMemberInputDto,
+    UpdateCastMemberOutputDto
+};
+
+class UpdateCastMemberUseCase
+{
+
+    protected $repository;
+
+    public function __construct(CastMemberRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function execute(UpdateCastMemberInputDto $input): UpdateCastMemberOutputDto
+    {
+        $castMember = $this->repository->findById($input->id);
+
+        $castMember->update($input->name);
+
+        $updatedCastMember = $this->repository->update($castMember);
+
+        return new UpdateCastMemberOutputDto(
+            id: $updatedCastMember->id(),
+            name: $updatedCastMember->name,
+            type: $updatedCastMember->type->value,
+            createdAt: $updatedCastMember->createdAt()
+        );
+    }
+
+}
