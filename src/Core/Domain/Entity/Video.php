a52b8d46f7e77c110311453e020e06ab60b5dff0
Vicente Junior <vicente19981@live.com>
2023-03-19 00:04:52 -0300

    feat: ensure Video attributes

---
+++ src/Core/Domain/Entity/Video.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Core\Domain\Entity;
+
+use Core\Domain\Entity\Traits\MethodsMagicsTrait;
+use Core\Domain\Enum\Rating;
+use Core\Domain\ValueObject\Uuid;
+
+class Video
+{
+    use MethodsMagicsTrait;
+
+    public function __construct(
+        protected string $title,
+        protected string $description,
+        protected int    $yearLaunched,
+        protected int    $duration,
+        protected bool   $opened,
+        protected Rating $rating,
+        protected ?Uuid  $id = null,
+        protected bool   $published = false,
+    )
+    {
+        $this->id = $this->id ?? Uuid::random();
+    }
+}
